78c1d532c44ce8228c3435e7050586fde29edbab
Said Aspen <mail@saidaspen.se>
2019-12-02 21:56:44 +0100

    Solution for day 2 in rust

--- rust/aoc02/src/main.rs
+++ rust/aoc02/src/main.rs
@@ -1,7 +1,7 @@
 use std::io;
 use std::io::Read;
 
-const PART: usize = 1;
+const PART: usize = 2;
 
 fn main() {
     let mut input = String::new();
@@ -11,31 +11,41 @@ fn main() {
     };
 
     let mut op_codes: Vec<usize> = input
-        .split(",")
+        .split(',')
         .map(|s| s.trim())
         .map(|s| s.parse::<usize>().unwrap())
         .collect();
-    println!("{:?}", op_codes);
 
     if PART == 1 {
         op_codes[1] = 12;
         op_codes[2] = 2;
-        println!("{}", run_program(op_codes));
+        println!("{}", run_program(&op_codes));
     } else {
-        println!("Part 2");
+        for noun in 0..99 {
+            for verb in 0..99 {
+                op_codes[1] = noun as usize;
+                op_codes[2] = verb as usize;
+                if run_program(&op_codes) == 19690720 {
+                    println!("{}", 100 * noun + verb);
+                }
+            }
+        }
     }
 }
 
-fn run_program(input2: Vec<usize>) -> usize {
+fn run_program(input2: &[usize]) -> usize {
     let mut input = input2.to_vec();
-    let pos = 0;
+    let mut pos = 0;
     loop {
+        let param1 = input[pos + 1];
+        let param2 = input[pos + 2];
+        let store_pos = input[pos + 3];
         match input[pos] {
             99 => return input[0],
-            1 => input[input[pos + 3]] = input[input[pos + 1]] + input[input[pos + 2]],
-            2 => input[input[pos + 3]] = input[input[pos + 1]] * input[input[pos + 2]],
+            1 => input[store_pos] = input[param1] + input[param2],
+            2 => input[store_pos] = input[param1] * input[param2],
             _ => panic!("Unsupported code"),
         }
-        pos = pos + 4;
+        pos += 4;
     }
 }
