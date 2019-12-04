74faae4a0ce5cf0d2c969fcd2e848161da229088
Said Aspen <mail@saidaspen.se>
2019-12-04 06:53:35 +0100

    AOC 04 Solution Java

---
+++ java/src/se/saidaspen/aoc2019/aoc04/Aoc04.java
@@ -0,0 +1,28 @@
+package se.saidaspen.aoc2019.aoc04;
+
+import java.io.IOException;
+import java.util.stream.IntStream;
+
+public class Aoc04 {
+
+    private static final int START_RANGE = 137683;
+    private static final int END_RANGE = 596253;
+
+    public static void main(String[] args) throws IOException {
+        System.out.println(IntStream.range(START_RANGE, END_RANGE).filter(Aoc04::meetCriteria).count());
+    }
+
+    public static boolean meetCriteria(int val) {
+        char[] chars = Integer.toString(val).toCharArray();
+        boolean allIncreasing = true;
+        for (int i = 0; i < chars.length - 1; i++) {
+            allIncreasing &= chars[i] <= chars[i + 1];
+        }
+        boolean hasTwoAdj = chars[0] == chars[1] && chars[1] != chars[2];
+        for (int i = 1; i < chars.length - 2; i++) {
+            hasTwoAdj |= chars[i] == chars[i + 1] && chars[i + 1] != chars[i + 2] && chars[i] != chars[i - 1];
+        }
+        hasTwoAdj |= chars[4] == chars[5] && chars[4] != chars[3];
+        return hasTwoAdj && allIncreasing;
+    }
+}
