48559c1b06d299f634c213821feb188817688cf7
Said Aspen <mail@saidaspen.se>
2019-12-04 14:31:24 +0100

    Update main.rs

--- rust/aoc04/src/main.rs
+++ rust/aoc04/src/main.rs
@@ -1,14 +1,11 @@
 fn main() {
-    println!(
-        "{}",
-        (137683..596253).filter(|i| is_cand(*i as i32)).count()
-    );
+    println!("{}", (137683..596253).filter(|i| is_cand(*i as i32)).count());
 }
 
 fn is_cand(i: i32) -> bool {
     let chars: Vec<char> = i.to_string().chars().collect();
-    chars.windows(2).all(|c| c[0] <= c[1])
-        && chars
-            .iter()
-            .any(|c| chars.iter().filter(|d| c == *d).count() == 2)
+    // 1. Check that it is sorted
+    // 2. Find any that has a two occurences.
+    chars.windows(2).all(|c| c[0] <= c[1]) &&  
+        chars.iter().any(|c| chars.iter().filter(|d| c == *d).count() == 2)
 }
