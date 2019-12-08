4a29896bee0049d5e3f14ad0e1a96547873a394d
Said Aspen <mail@saidaspen.se>
2019-12-08 05:39:20 +0100

    WIP commit

---
+++ java/test/se/saidaspen/aoc2019/aoc07/Aoc07Test.java
@@ -0,0 +1,7 @@
+package se.saidaspen.aoc2019.aoc07;
+
+import static org.junit.Assert.*;
+
+public class Aoc07Test {
+
+}
--- rust/aoc05/src/main.rs
+++ rust/aoc05/src/main.rs
@@ -26,23 +26,23 @@ fn run_program(input2: &[i32]) {
             return;
         } else if op_code == "03" {
             let pos = input[pc + 1] as usize;
-            input[pos] = 1;
-            pc += 2; // This is speial input
+            input[pos] = 5; // Special input
+            pc += 2;
             continue;
         } else if op_code == "04" {
             let pos = input[pc + 1] as usize;
-            println!("{}", input[pos as usize]);
+            println!("Output {}", input[pos]);
             pc += 2;
             continue;
         }
-        let param1 = match cmd.chars().nth(3).unwrap() {
-            '0' => input[pc + 1],
-            '1' => input[input[pc + 1] as usize],
+        let param1 = match cmd.chars().nth(2).unwrap() {
+            '1' => input[pc + 1],
+            '0' => input[input[pc + 1] as usize],
             _ => panic!("Unexpected mode"),
         };
-        let param2 = match cmd.chars().nth(2).unwrap() {
-            '0' => input[pc + 2],
-            '1' => input[input[pc + 2] as usize],
+        let param2 = match cmd.chars().nth(1).unwrap() {
+            '1' => input[pc + 2],
+            '0' => input[input[pc + 2] as usize],
             _ => panic!("Unexpected mode"),
         };
         let store_pos = input[pc + 3] as usize;
@@ -53,11 +53,41 @@ fn run_program(input2: &[i32]) {
         match op_code {
             "99" => return,
             "01" => {
-                input[store_pos] = input[param1 as usize] + input[param2 as usize];
+                input[store_pos] = param1 + param2;
                 pc += 4;
             }
             "02" => {
-                input[store_pos] = input[param1 as usize] * input[param2 as usize];
+                input[store_pos] = param1 * param2;
+                pc += 4;
+            }
+            "05" => {
+                if param1 > 0 {
+                    pc = param2 as usize;
+                } else {
+                    pc += 3;
+                }
+            }
+            "06" => {
+                if param1 == 0 {
+                    pc = param2 as usize;
+                } else {
+                    pc += 3;
+                }
+            }
+            "07" => {
+                if param1 < param2 {
+                    input[store_pos] = 1;
+                } else {
+                    input[store_pos] = 0;
+                }
+                pc += 4;
+            }
+            "08" => {
+                if param1 == param2 {
+                    input[store_pos] = 1;
+                } else {
+                    input[store_pos] = 0;
+                }
                 pc += 4;
             }
             code => panic!("Unsupported code {}", code),
