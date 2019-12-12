189d552840bbfeb75dab6c88147476eb3f5339d2
Said Aspen <mail@saidaspen.se>
2019-12-12 05:58:50 +0100

    Temporary WIP Commit (yes, I know thats not great) of AOC09 in Rust

--- rust/aoc09/src/main.rs
+++ rust/aoc09/src/main.rs
@@ -1,5 +1,5 @@
-use itertools::Itertools;
 use std::cmp;
+use std::collections::HashMap;
 use std::io;
 use std::io::Read;
 
@@ -20,8 +20,9 @@ fn main() {
 
 #[derive(Debug)]
 struct IcProcessor {
-    code: Vec<i32>,
-    pc: usize,
+    mem: HashMap<i32, i32>,
+    pc: i32,
+    rbo: i32,
     status: Status,
     input: Vec<i32>,
     output: Vec<i32>,
@@ -39,106 +40,94 @@ impl IcProcessor {
         return val;
     }
 
-    fn get_param(cmd: &str, i: usize) -> usize {
-        0
+    fn param_addr(&mut self, cmd: &str, i: usize) -> i32 {
+        let c = cmd.chars().nth(3 - i).unwrap();
+        match c {
+            '0' => self.load(self.pc + 1),
+            '1' => self.pc + 1,
+            '2' => self.load(self.pc + 1) + self.rbo,
+        }
+    }
+
+    fn load(&mut self, i: i32) -> i32 {
+        match self.mem.get(&i) {
+            Some(v) => *v,
+            None => 0,
+        }
+    }
+
+    fn set(&mut self, pos: i32, val: i32) {
+        self.mem.insert(pos, val);
     }
 
     fn run(&mut self) {
         loop {
-            let cmd = format!("{:0>5}", self.code[self.pc].to_string());
+            let cmd = format!("{:0>5}", self.load(self.pc).to_string());
             let op_code = &cmd[3..];
-            let p1Addr = param_addr(cmd, 1);
-            let p2Addr = param_addr(cmd, 2);
-            let p3Addr = param_addr(cmd, 3);
-
+            let p1Addr = self.param_addr(&cmd, 1);
+            let p2Addr = self.param_addr(&cmd, 2);
+            let p3Addr = self.param_addr(&cmd, 3);
             match op_code {
                 "99" => {
                     self.status = Status::Halted;
                     return;
                 }
-                "01" => {}
-                "02" => {}
-                "03" => {}
-                "04" => {}
-                "05" => {}
-                "06" => {}
-                "07" => {}
-                "08" => {}
-                code => panic!("Unexepected op code recieved {}", code),
-            }
-
-            if op_code == "99" {
-                self.status = Status::Halted;
-                return;
-            } else if op_code == "03" {
-                let pos = self.code[self.pc + 1] as usize;
-                if self.input.is_empty() {
-                    self.status = Status::Waiting;
-                    return;
-                }
-                let in_val = self.input.remove(0);
-                self.code[pos] = in_val;
-                self.status = Status::Runnable;
-                self.pc += 2;
-                continue;
-            } else if op_code == "04" {
-                let pos = self.code[self.pc + 1] as usize;
-                let out_val = self.code[pos as usize];
-                self.output.push(out_val);
-                self.pc += 2;
-                continue;
-            }
-            let param1 = match cmd.chars().nth(2).unwrap() {
-                '1' => self.code[self.pc + 1],
-                '0' => self.code[self.code[self.pc + 1] as usize],
-                _ => panic!("Unexpected mode"),
-            };
-            let param2 = match cmd.chars().nth(1).unwrap() {
-                '1' => self.code[self.pc + 2],
-                '0' => self.code[self.code[self.pc + 2] as usize],
-                _ => panic!("Unexpected mode"),
-            };
-            let store_pos = self.code[self.pc + 3] as usize;
-            match op_code {
                 "01" => {
-                    self.code[store_pos] = param1 + param2;
+                    self.set(self.load(p3Addr), self.load(p1Addr) + self.load(p2Addr));
                     self.pc += 4;
                 }
                 "02" => {
-                    self.code[store_pos] = param1 * param2;
+                    self.set(self.load(p3Addr), self.load(p1Addr) * self.load(p2Addr));
                     self.pc += 4;
                 }
+                "03" => {
+                    let pos = self.load(self.pc + 1);
+                    if self.input.is_empty() {
+                        self.status = Status::Waiting;
+                        return;
+                    }
+                    let in_val = self.input.remove(0);
+                    self.set(pos, in_val);
+                    self.status = Status::Runnable;
+                    self.pc += 2;
+                }
+                "04" => {
+                    let val = self.load(p1Addr);
+                    self.output.push(val);
+                    self.pc += 2;
+                }
                 "05" => {
-                    if param1 > 0 {
-                        self.pc = param2 as usize;
+                    if self.load(p1Addr) > 0 {
+                        self.pc = self.load(p2Addr);
                     } else {
                         self.pc += 3;
                     }
                 }
                 "06" => {
-                    if param1 == 0 {
-                        self.pc = param2 as usize;
+                    if self.load(p1Addr) == 0 {
+                        self.pc = self.load(p2Addr);
                     } else {
                         self.pc += 3;
                     }
                 }
                 "07" => {
-                    if param1 < param2 {
-                        self.code[store_pos] = 1;
-                    } else {
-                        self.code[store_pos] = 0;
-                    }
+                    let mut val: i32 = match self.load(p1Addr) < self.load(p2Addr) {
+                        true => 1,
+                        false => 0,
+                    };
+                    let pos = self.load(p3Addr);
+                    self.set(pos, val);
                     self.pc += 4;
                 }
                 "08" => {
-                    if param1 == param2 {
-                        self.code[store_pos] = 1;
-                    } else {
-                        self.code[store_pos] = 0;
-                    }
+                    let mut val: i32 = match self.load(p1Addr) == self.load(p2Addr) {
+                        true => 1,
+                        false => 0,
+                    };
+                    let pos = self.load(p3Addr);
+                    self.set(pos, val);
                     self.pc += 4;
                 }
-
                 code => panic!("Unsupported code {}", code),
             };
         }
