316cc4db9ba631323e0b4424b1bc689d8f48f478
Said Aspen <mail@saidaspen.se>
2019-12-25 11:28:14 +0100

    Refactored D1 Java

--- java/src/se/saidaspen/aoc2019/aoc01/Day01.java
+++ java/src/se/saidaspen/aoc2019/aoc01/Day01.java
@@ -9,9 +9,7 @@ import java.util.Arrays;
 import static java.lang.Integer.parseInt;
 import static java.lang.Math.floor;
 
-public class Day01 implements Day {
-
-    private static final String NEW_LINE = "\n";
+public final class Day01 implements Day {
 
     private final String input;
 
@@ -19,20 +17,20 @@ public class Day01 implements Day {
         this.input = input;
     }
 
-    private static int fuelFor(int d, boolean recursive) {
-        int fuelReq = (int) floor((double) d / 3) - 2;
-        return fuelReq < 1 ? 0 : !recursive ? fuelReq : fuelReq + fuelFor(fuelReq, true);
-    }
-
     @Override
     public Answer part1() {
-        return StringAnswer.of(Integer.toString(Arrays.stream(input.split(NEW_LINE))
+        return StringAnswer.of(Integer.toString(Arrays.stream(input.split("\n"))
                 .mapToInt(d -> Day01.fuelFor(parseInt(d), false)).sum()));
     }
 
     @Override
     public Answer part2() {
-        return StringAnswer.of(Integer.toString(Arrays.stream(input.split(NEW_LINE))
+        return StringAnswer.of(Integer.toString(Arrays.stream(input.split("\n"))
                 .mapToInt(d -> Day01.fuelFor(parseInt(d), true)).sum()));
+    }
+
+    private static int fuelFor(int d, boolean recursive) {
+        int fuelReq = (int) floor((double) d / 3) - 2;
+        return fuelReq < 1 ? 0 : !recursive ? fuelReq : fuelReq + fuelFor(fuelReq, true);
     }
 }
